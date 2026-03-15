a3931d0df65e8827e69109fe67b2518966683a13
Ihor Zasimenko <boxletter2011@gmail.com>
2026-03-15 11:00:46 +0200

    Fix flake8 E501 line length issues in admin.py and urls.py

--- user/admin.py
+++ user/admin.py
@@ -10,14 +10,33 @@ class UserAdmin(BaseUserAdmin):
     list_display = ["email", "is_staff", "is_active"]
     fieldsets = (
         (None, {"fields": ("email", "password")}),
-        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser",
-                                       "groups", "user_permissions")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
     add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("email", "password1", "password2", "is_staff", "is_active"),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
     )
     search_fields = ["email"]
